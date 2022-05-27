fe7f6764f96657bbf20bd731a74d3ea77db9372d
Daniel Byrne <dbyrne978@gmail.com>
2022-05-27 18:49:01 -0400

    Create getPlayerSelection function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,7 +5,6 @@ let computerPlay = function() {
 };
 
 let playRound = function(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) return "tie";
     switch (playerSelection) {
         case "rock":
@@ -15,4 +14,29 @@ let playRound = function(playerSelection, computerSelection) {
         case "scissors":
             return (computerSelection == "rock") ? "lose" : "win";
     }
+};
+
+let game = function() {
+    let gameMsg = "Let's play 5 rounds of Rock, Paper, Scissors!\n" +
+        "Please enter rock, paper, or scissors:\n";
+
+    for (let i = 0; i < 5; i++) {
+        getPlayerSelection(gameMsg);
+        // determineWinner
+        //   tbd...
+        // updateResults
+        gameMsg = gameMsg + `Round ${i+1}: PLACEHOLDER RESULT\n`;
+    };
+    // calculate final results and display
+    //   tbd...
+    // alert(`Results:\n${results}`);
+};
+
+let getPlayerSelection = function(gameMsg) {
+    let playerSelection;
+    while(!RpsArray.includes(playerSelection)) {
+        playerSelection = prompt(gameMsg);
+        if (playerSelection === null) return;
+        playerSelection = playerSelection.toLowerCase();
+    };
 };
