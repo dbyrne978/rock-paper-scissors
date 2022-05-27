42752c1b26c0d6e99797f7841d843b0b21131b6c
Daniel Byrne <dbyrne978@gmail.com>
2022-05-27 15:10:23 -0400

    Create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,18 @@
-let RpsArray = ["Rock","Paper","Scissors"];
+let RpsArray = ["rock", "paper", "scissors"];
 
 let computerPlay = function() {
     return RpsArray[Math.floor(Math.random() * 3)];
 };
 
+let playRound = function(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) return "tie";
+    switch (playerSelection) {
+        case "rock":
+            return (computerSelection == "paper") ? "lose" : "win";
+        case "paper":
+            return (computerSelection == "scissors") ? "lose" : "win";
+        case "scissors":
+            return (computerSelection == "rock") ? "lose" : "win";
+    }
+};
